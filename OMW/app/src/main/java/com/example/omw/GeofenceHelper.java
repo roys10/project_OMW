17bd03d4d7326346ec767cfacc318b50327d9e4d
roys10 <roysegev10@gmail.com>
2021-01-22 13:24:48 +0200

    ver1

---
+++ OMW/app/src/main/java/com/example/omw/GeofenceHelper.java
@@ -0,0 +1,58 @@
+package com.example.omw;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.maps.model.LatLng;
+
+public class GeofenceHelper extends ContextWrapper {
+    private static final String TAG = "GeofenceHelper";
+    PendingIntent pi;
+
+    public GeofenceHelper(Context base) {
+        super(base);
+    }
+
+    public GeofencingRequest getGeofencingRequest(Geofence geofence){
+        return new GeofencingRequest.Builder().addGeofence(geofence).setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER).build();
+    }
+
+    public Geofence getGeofence(String id, LatLng latlng, float radius, int transitionTypes ){
+
+        return new Geofence.Builder().setCircularRegion(latlng.latitude, latlng.longitude, radius).setRequestId(id).setTransitionTypes(transitionTypes).setLoiteringDelay(5000).setExpirationDuration(Geofence.NEVER_EXPIRE).build();
+    }
+
+    public PendingIntent getPendingIntent(){
+        if (pi!=null){
+            return pi;
+        }
+        Intent intent= new Intent(this, GeofenceBroadcastReceiver.class);
+        pi=PendingIntent.getBroadcast(this, 2607, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+        return  pi;
+    }
+
+    public String getErrorString(Exception e){
+        if(e instanceof ApiException){
+            ApiException apiException= (ApiException) e;
+            switch (apiException.getStatusCode()){
+                case GeofenceStatusCodes
+                        .GEOFENCE_NOT_AVAILABLE:
+                    return "GEOFENCE_NOT_AVAILABLE";
+                case GeofenceStatusCodes
+                        .GEOFENCE_TOO_MANY_GEOFENCES:
+                    return "GEOFENCE_TOO_MANY_GEOFENCES";
+                case GeofenceStatusCodes
+                        .GEOFENCE_TOO_MANY_PENDING_INTENTS:
+                    return "GEOFENCE_TOO_MANY_PENDING_INTENTS";
+            }
+        }
+        return  e.getLocalizedMessage();
+    }
+
+}
